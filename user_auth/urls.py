0e9bea8bafbe478ed46dcdb43c84b05339e10ae0
abedalkarim99 <abedalkarim215@gmail.com>
2020-03-19 16:05:24 +0200

    create the sign up page template and the view function for it

--- user_auth/urls.py
+++ user_auth/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('',home,name='home'),
     path('login/',login_user,name='login_user'),
     path('logout/',logout_user,name='logout_user'),
+    path('sign_up/',sign_up_user,name='sign_up_user'),
 ]
--- user_auth/views.py
+++ user_auth/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render ,redirect
 from django.contrib.auth import login , authenticate ,logout
 from django.contrib import messages
+from django.contrib.auth.models import User
 
 
 def home(request) :
@@ -29,3 +30,40 @@ def logout_user(request) :
     if request.method == "POST" :
         logout(request)
         return redirect('home')
+
+def sign_up_user(request) :
+    if request.method == "GET" :
+        return render(request, 'user_auth/sign_up.html')
+    elif request.method == "POST" :
+        user_first_name = request.POST['first_name']
+        user_last_name = request.POST['last_name']
+        user_username = request.POST['username']
+        user_email = request.POST['email']
+        user_password1 = request.POST['password1']
+        user_password2 = request.POST['password2']
+
+    if User.objects.filter(username=user_username).exists():
+        messages.info(request, 'username is taken, try another one')
+        return redirect('sign_up_user')
+
+
+
+    elif User.objects.filter(email= user_email).exists() :
+        messages.info(request, 'email is taken, try another one')
+        return redirect('sign_up_user')
+
+    elif user_password1 != user_password2 :
+        messages.info(request, 'password not matching')
+        return redirect('sign_up_user')
+
+
+    else :
+        user = User.objects.create_user(first_name = user_first_name,
+                                        last_name = user_last_name,
+                                        username = user_username,
+                                        email = user_email,
+                                        password = user_password1
+                                        )
+        user.save();
+        login(request,user)
+        return redirect("home")
