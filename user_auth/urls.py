ba467146e8f2256ddc530b793a57538b2a18ed8b
abedalkarim99 <abedalkarim215@gmail.com>
2020-03-25 00:32:22 +0200

    add the change password functionality

--- user_auth/urls.py
+++ user_auth/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path('logout/',logout_user,name='logout_user'),
     path('sign_up/',sign_up_user,name='sign_up_user'),
     path('delete_account/<int:user_id>/',delete_account,name='delete_account'),
+    path('change_password/',change_password,name='change_password'),
 
     # urls for Generate Password function
     path('generate_password/', generate_password, name='generate_password'),
--- user_auth/views.py
+++ user_auth/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render ,redirect , get_object_or_404
-from django.contrib.auth import login , authenticate ,logout
+from django.contrib.auth import login , authenticate ,logout ,update_session_auth_hash
 from django.contrib import messages
 from django.contrib.auth.models import User
-import random
 from django.contrib.auth.decorators import login_required
+
+# imports for generate_password function
+import random
 
 
 
@@ -53,31 +55,31 @@ def sign_up_user(request) :
         user_password1 = request.POST['password1']
         user_password2 = request.POST['password2']
 
-    if User.objects.filter(username=user_username).exists():
-        messages.info(request, 'username is taken, try another one')
-        return redirect('sign_up_user')
+        if User.objects.filter(username=user_username).exists():
+            messages.info(request, 'username is taken, try another one')
+            return redirect('sign_up_user')
 
 
 
-    elif User.objects.filter(email= user_email).exists() :
-        messages.info(request, 'email is taken, try another one')
-        return redirect('sign_up_user')
+        elif User.objects.filter(email= user_email).exists() :
+            messages.info(request, 'email is taken, try another one')
+            return redirect('sign_up_user')
 
-    elif user_password1 != user_password2 :
-        messages.info(request, 'password not matching')
-        return redirect('sign_up_user')
+        elif user_password1 != user_password2 :
+            messages.info(request, 'password not matching')
+            return redirect('sign_up_user')
 
 
-    else :
-        user = User.objects.create_user(first_name = user_first_name,
-                                        last_name = user_last_name,
-                                        username = user_username,
-                                        email = user_email,
-                                        password = user_password1
-                                        )
-        user.save();
-        login(request,user)
-        return redirect("home")
+        else :
+            user = User.objects.create_user(first_name = user_first_name,
+                                            last_name = user_last_name,
+                                            username = user_username,
+                                            email = user_email,
+                                            password = user_password1
+                                            )
+            user.save()
+            login(request,user)
+            return redirect("home")
 
 
 @login_required
@@ -87,6 +89,32 @@ def delete_account(request,user_id) :
         user.delete()
         messages.info(request,"Your account was deleted sucssfully")
         return redirect('home')
+
+@login_required
+def change_password(request) :
+    if request.method == "GET" :
+        return render(request,'user_auth/change_password.html')
+    if request.method == "POST" :
+        user = get_object_or_404(User, pk=request.user.id)
+        user_old_password = request.POST['old_password']
+        user_new_password = request.POST['new_password']
+        user_new_password2 = request.POST['new_password2']
+
+        if not user.check_password(user_old_password):
+            messages.info(request, 'the password(current) You entered is incorrect')
+            return redirect('change_password')
+        elif user_new_password != user_new_password2 :
+            messages.info(request, 'the new password did not matching')
+            return redirect('change_password')
+        else :
+            user.set_password(user_new_password)
+            user.save()
+            # update_session_auth_hash(request, user)
+            messages.info(request, "Your password was changed sucssfully , please login again")
+            return redirect('home')
+
+
+
 
 # generate_password function view
 def generate_password(request) :
